5ffdfe4760942d1b7389ddc9a1a8a8a9a24c2bce
Muhirsan Dev <muhirsanbusines@gmail.com>
2026-09-20 14:50:18 +0700

    refactor(Product): update fallback image to real product image

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -67,7 +67,7 @@ class Product extends Model
     public function getImageUrlAttribute(): string
     {
         if (!$this->image) {
-            return asset('images/sample/denim_jacket.jpg');
+            return asset('storage/products/34.000.jpeg');
         }
 
         if (str_starts_with($this->image, 'http://') || str_starts_with($this->image, 'https://')) {
@@ -78,13 +78,13 @@ class Product extends Model
             if (file_exists(public_path('storage/' . $this->image))) {
                 return asset('storage/' . $this->image);
             }
-            return asset('images/sample/denim_jacket.jpg');
+            return asset('storage/products/34.000.jpeg');
         }
 
         if (file_exists(public_path($this->image))) {
             return asset($this->image);
         }
 
-        return asset('images/sample/denim_jacket.jpg');
+        return asset('storage/products/34.000.jpeg');
     }
 }
