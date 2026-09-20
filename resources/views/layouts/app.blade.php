206742b2e6e8866d8227339958edea3ec008b77e
Muhirsan Dev <muhirsanbusines@gmail.com>
2026-09-20 17:26:38 +0700

    Update default page title and meta description in layout to VYORA

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,11 @@
 <html lang="id">
 
 <head>
-  <title>@yield('title', 'Vyora - Fashion Store')</title>
+  <title>@yield('title', 'VYORA')</title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="Vyora Fashion Store - Temukan koleksi pakaian terkini dan belanja mudah via WhatsApp.">
+  <meta name="description" content="VYORA - Temukan koleksi tas & dompet elegan terkini dengan belanja mudah via WhatsApp.">
   
   @php
     $faviconUrl = \App\Models\Setting::getFaviconUrl();
