2d5622f94f1c7cdacb84c89c967e682cf38aae33
Muhirsan Dev <muhirsanbusines@gmail.com>
2026-09-20 14:56:05 +0700

    feat(settings): update store address and whatsapp number to official contacts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,8 +38,8 @@ class DatabaseSeeder extends Seeder
         // 2. Seed Settings
         Setting::set('store_name', 'Vyora Fashion Store');
         Setting::set('store_email', 'info@vyorastore.com');
-        Setting::set('store_address', 'Jakarta, Indonesia');
-        Setting::set('whatsapp_number', '6281234567890');
+        Setting::set('store_address', 'Jl. Raya Rangkasbitung No. 8, Kareo, Serang, Kabupaten Serang, Banten 42177');
+        Setting::set('whatsapp_number', '6281994578184');
         Setting::set('whatsapp_message', 'Halo Admin Vyora, saya tertarik untuk memesan produk berikut:');
 
         // 3. Seed Categories (Using real product images)
